ea3d9141394e0a2609662521f5c00a72bfc3ae7d
floravel <flo-hess1@web.de>
2020-12-23 15:07:25 +0100

    set up tailwind css

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="home">
-    <p>Whats UPPPPP!?</p>
+    <custom-button/>
+    <custom-button dark/>
+    <custom-button/>
   </div>
 </template>
 
 <script>
 // eslint-disable-next-line no-unused-vars
 import axios from 'axios'
+import CustomButton from '../components/Button'
 
 export default {
   name: 'Home',
+  components: {
+    CustomButton
+  },
   mounted () {
     axios.get('http://zeart.test/artworks').then(response => {
       console.log(response.data)
